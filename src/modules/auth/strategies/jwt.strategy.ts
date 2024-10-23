4b5f1786a0db3bbd9b13b67e7fcf717307d07d45
root <laguta.d.s@gmail.com>
2024-10-23 04:55:27 +0300

    fix/cookie-vercel-5

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,12 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Request } from 'express';
-import { UserRepository } from '../../../infrastructure/user.repository';
+import { UserRepository } from 'src/infrastructure/user.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly userRepository: UserRepository,
-    private readonly logger: Logger) {
+  constructor(private readonly userRepository: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
@@ -19,17 +18,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: { userId: number }) {
-    this.logger.log('payload', payload)
-    // try {
-    const user = await this.userRepository.findUserById(payload.userId);
-    this.logger.log('user', user)
-    return { email: '23123@gmail.com', password: '123123' }
-    // } catch (e) {
-    // return { email: '1123123@gij.com' }
-    // throw new HttpException(
-    //   'User with this id does not exist',
-    //   HttpStatus.NOT_FOUND,
-    // );
-    // }
+    try {
+      return this.userRepository.findUserById(payload.userId);
+    } catch (e) {
+      throw new HttpException(
+        'User with this id does not exist',
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 }
